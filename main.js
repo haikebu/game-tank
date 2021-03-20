a5cb4922f5a5faaf1cf0c806e792e10410c7d54d
Haikebu <bhhai1610@gmail.com>
2021-03-20 18:09:48 +0700

    bi Hit thi thanh hp giam va doi mau

--- main.js
+++ main.js
@@ -5,23 +5,29 @@ function getMineTop() {
   return mineTop
 }
 function getMineLeft() {
-    let mineLeft = document.getElementById("boom").style["top"]
+    let mineLeft = document.getElementById("boom").style["left"]
     mmineLeft = parseInt(mineLeft)
     return mineLeft
 }
 function moveTank(e) {
 let keyCode = e.keyCode
-if (keyCode == 38) {
+switch (keyCode){
+    case (38) : {
     moveUp()
-    }
-else if (keyCode == 40) {
+    break
+        }
+    case (40) : {
     moveDown()
-    }
-else if (keyCode == 37) {
+    break
+        }
+    case (37) : {
     moveLeft()
-    }
-else if (keyCode == 39) {
+    break
+        }
+    case (39) : {
     moveRight()
+    break
+        }
     }
 }
 function moveDown() {
@@ -63,16 +69,86 @@ if (left<270) {
 function hit() {
     hp -= 1;
     document.getElementById("hp").innerText = hp
-    if (hp == 0) {
-        document.getElementById("tank").src = "explosion.png"  
+switch(hp){
+    case (19) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 190px"
+        break
+        }
+    case (18) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 180px"
+        break
+        }
+    case (17) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 170px"
+        break
+        }
+    case (16) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 160px"
+        break
+        }
+    case (15) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 150px"
+        break
+        }
+    case (14) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 140px"
+        break
+        }
+    case (13) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 130px"
+        break
+     }
+    case (12) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 120px"
+        break
+        }
+    case (11) : {
+        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 110px"
+        break
+        }
+    case (10) : {
+        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 100px"
+        break
+        }
+    case (9) : {
+        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 90px"
+        break
+        }
+    case (8) : {
+        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 80px"
+        break
+        }
+    case (7) : {
+        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 70px"
+        break
+        }
+    case (6) : {
+        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 60px"
+        break
+        }
+    case (5) : {
+        document.getElementById("hp").style = "background-color: red; height: 30px; width: 50px"
+        break
+        }
+    case (4) : {
+        document.getElementById("hp").style = "background-color: red; height: 30px; width: 40px"
+        break
+        }
+    case (3) : {
+        document.getElementById("hp").style = "background-color: red; height: 30px; width: 30px"
+        break
+        }
+    case (2) : {
+        document.getElementById("hp").style = "background-color: red; height: 30px; width: 20px"
+        break
+        }
+    case (1) : {
+        document.getElementById("hp").style = "background-color: red; height: 30px; width: 10px"
+        break
+        }
+    case (0) : {
+        document.getElementById("tank").src = "explosion.png"
+        break
+        }
     }
 }
-    let curTop = parseInt(document.getElementById("tank").style["top"]) 
-    let curLeft = parseInt(document.getElementById("tank").style["left"])
-    let mineTop = getMineTop()
-    let mineLeft = getMineTop()
-    console.log(curTop)
-    if ((curTop == mineTop) & (curLeft == mineLeft)) {
-        document.getElementById("tank").src = "explosion.png"
-        document.getElementById("boom").style.visibility = "hidden"
-    }
