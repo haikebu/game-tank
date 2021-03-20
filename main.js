1a051d7138975e16d367881fca55495a85f9d38b
Haikebu <bhhai1610@gmail.com>
2021-03-20 20:59:31 +0700

    sua lai code

--- main.js
+++ main.js
@@ -1,144 +1,120 @@
 let hp = 20;
 function moveTank(e) {
-let keyCode = e.keyCode
-switch (keyCode){
-    case (38) : {
-    moveUp()
-    break
-        }
-    case (40) : {
-    moveDown()
-    break
-        }
-    case (37) : {
-    moveLeft()
-    break
-        }
-    case (39) : {
-    moveRight()
-    break
-        }
+    let keyCode = e.keyCode
+    switch (keyCode){
+        case 38 : moveUp()
+        break
+        case 40 : moveDown()
+        break
+        case 37 : moveLeft()
+        break
+        case 39 : moveRight()
+        break
     }
 }
 function moveDown() {
-let top = document.getElementById("tank").style["top"];
-top = parseInt(top);
-if (top < 270) {
-    top = top + 30;
-    document.getElementById("tank").style["top"] = `${top}px`;
-    document.getElementById("tank").style["transform"] = "rotate(180deg)"
+    let top = document.getElementById("tank").style["top"];
+    top = parseInt(top);
+    if (top < 270) {
+        top = top + 30;
+        document.getElementById("tank").style["top"] = `${top}px`;
+        document.getElementById("tank").style["transform"] = "rotate(180deg)"
     } 
 }
 function moveUp() {
-let top = document.getElementById("tank").style["top"];
-top = parseInt(top);
-if (top > 0) {
-    top = top - 30;
-    document.getElementById("tank").style["top"] = `${top}px`;
-    document.getElementById("tank").style["transform"] = "rotate(0deg)"
+    let top = document.getElementById("tank").style["top"];
+    top = parseInt(top);
+    if (top > 0) {
+        top = top - 30;
+        document.getElementById("tank").style["top"] = `${top}px`;
+        document.getElementById("tank").style["transform"] = "rotate(0deg)"
     }
 }
 function moveLeft() {
-let left = document.getElementById("tank").style["left"];
-left = parseInt(left);
-if (left > 0) {
-    left = left - 30;
-    document.getElementById("tank").style["left"] = `${left}px`;
-    document.getElementById("tank").style["transform"] = "rotate(-90deg)"
+    let left = document.getElementById("tank").style["left"];
+    left = parseInt(left);
+    if (left > 0) {
+        left = left - 30;
+        document.getElementById("tank").style["left"] = `${left}px`;
+        document.getElementById("tank").style["transform"] = "rotate(-90deg)"
     }   
 }
 function moveRight() {
-let left = document.getElementById("tank").style["left"];
-left = parseInt(left);
-if (left<270) {
-    left = left + 30;
-    document.getElementById("tank").style["left"] = `${left}px`;
-    document.getElementById("tank").style["transform"] = "rotate(90deg)"
+    let left = document.getElementById("tank").style["left"];
+    left = parseInt(left);
+    if (left<270) {
+        left = left + 30;
+        document.getElementById("tank").style["left"] = `${left}px`;
+        document.getElementById("tank").style["transform"] = "rotate(90deg)"
     }
 }
 function hit() {
-    hp -= 1;
+    hp -= 1; 
     document.getElementById("hp").innerText = hp
-switch(hp){
-    case (19) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 190px"
+    let thanhmau
+    let mau
+    switch(hp){
+        case 19 : thanhmau = hp*10
+            mau = "green"      
         break
-        }
-    case (18) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 180px"
-        break
-        }
-    case (17) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 170px"
-        break
-        }
-    case (16) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 160px"
-        break
-        }
-    case (15) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 150px"
-        break
-        }
-    case (14) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 140px"
-        break
-        }
-    case (13) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 130px"
-        break
-     }
-    case (12) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 120px"
-        break
-        }
-    case (11) : {
-        document.getElementById("hp").style = "background-color: greenyellow; height: 30px; width: 110px"
-        break
-        }
-    case (10) : {
-        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 100px"
-        break
-        }
-    case (9) : {
-        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 90px"
-        break
-        }
-    case (8) : {
-        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 80px"
-        break
-        }
-    case (7) : {
-        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 70px"
-        break
-        }
-    case (6) : {
-        document.getElementById("hp").style = "background-color: yellow; height: 30px; width: 60px"
-        break
-        }
-    case (5) : {
-        document.getElementById("hp").style = "background-color: red; height: 30px; width: 50px"
-        break
-        }
-    case (4) : {
-        document.getElementById("hp").style = "background-color: red; height: 30px; width: 40px"
-        break
-        }
-    case (3) : {
-        document.getElementById("hp").style = "background-color: red; height: 30px; width: 30px"
-        break
-        }
-    case (2) : {
-        document.getElementById("hp").style = "background-color: red; height: 30px; width: 20px"
-        break
-        }
-    case (1) : {
-        document.getElementById("hp").style = "background-color: red; height: 30px; width: 10px"
-        break
-        }
-    case (0) : {
-        document.getElementById("tank").src = "explosion.png"
-        break
-        }
+        case 18 : thanhmau = hp*10
+        mau = "green"
+            break
+        case 17 : thanhmau = hp*10
+        mau = "green"
+            break
+        case 16 : thanhmau = hp*10
+        mau = "green"
+            break
+        case 15 : thanhmau = hp*10
+        mau = "greenyellow"
+            break
+        case 14 :thanhmau = hp*10
+        mau = "greenyellow"
+            break
+        case 13 : thanhmau = hp*10
+        mau = "greenyellow"
+            break
+        case 12 : thanhmau = hp*10
+        mau = "greenyellow"
+            break
+        case 11 : thanhmau = hp*10
+        mau = "greenyellow"
+            break
+        case 10 : thanhmau = hp*10
+        mau = "yellow"
+            break
+        case 9 : thanhmau = hp*10
+        mau = "yellow"
+            break
+        case 8 : thanhmau = hp*10
+        mau = "yellow"
+            break
+        case 7 : thanhmau = hp*10
+        mau = "yellow"
+            break
+        case 6 : thanhmau = hp*10
+        mau = "yellow"
+            break
+        case 5 : thanhmau = hp*10
+        mau = "red"
+            break
+        case 4 : thanhmau = hp*10
+        mau = "red"
+            break
+        case 3 : thanhmau = hp*10
+        mau = "red"
+            break
+        case 2 : thanhmau = hp*10
+        mau = "red"
+            break
+        case 1 : thanhmau = hp*10
+        mau = "red"
+            break
+        case 0 : 
+            document.getElementById("tank").src = "explosion.png"
+            break
     }
+    document.getElementById("hp").style["width"] = `${thanhmau}px`
+    document.getElementById("hp").style["background-color"] = mau
 }
